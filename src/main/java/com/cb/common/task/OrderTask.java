a21e16ad6873ab18acfe3ee46ae84ac9ea64815c
CB <2023208906@qq.com>
2024-04-07 20:59:00 +0800

    2024.4.7 完成SpringTask定时任务

---
+++ src/main/java/com/cb/common/task/OrderTask.java
@@ -0,0 +1,79 @@
+package com.cb.common.task;
+
+
+import com.cb.mapper.OrderMapper;
+import com.cb.pojo.entity.Orders;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 定时任务类，定时处理订单状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    public static final String KEY = "shop:SHOP_STATUS";
+    /**
+     * 处理超时订单的方法
+     */
+    @Scheduled(cron = "0 * * * * ? ") //每分钟触发一次
+    public void processTimeoutOrder(){
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    /**
+     * 处理一直处于派送中状态的订单
+     */
+    @Scheduled(cron = "0 0 1 * * ?") //每天凌晨1点触发一次
+    public void processDeliveryOrder(){
+        log.info("定时处理处于派送中的订单：{}",LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    //每天23点关闭店铺
+    @Scheduled(cron = "0 0 23 * * ? *")
+    public void closeShop(){
+        redisTemplate.opsForValue().set(KEY,0);
+    }
+
+    //每天9点开启店铺
+    @Scheduled(cron = "0 0 9 * * ? *")
+    public void openShop(){
+        redisTemplate.opsForValue().set(KEY,0);
+    }
+}
