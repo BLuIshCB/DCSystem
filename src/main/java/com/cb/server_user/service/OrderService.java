7446761a06c55203c2f34f736f043adc4a9f49ea
CB <2023208906@qq.com>
2024-04-10 15:25:54 +0800

    2024.4.10 完成取完成订单功能

--- src/main/java/com/cb/server_user/service/OrderService.java
+++ src/main/java/com/cb/server_user/service/OrderService.java
@@ -483,28 +483,28 @@ public class OrderService  {
         orderMapper.update(orders);
     }
 
-//    /**
-//     * 完成订单
-//     *
-//     * @param id
-//     */
-//    public void complete(Long id) {
-//        // 根据id查询订单
-//        Orders ordersDB = orderMapper.getById(id);
-//
-//        // 校验订单是否存在，并且状态为4
-//        if (ordersDB == null || !ordersDB.getStatus().equals(Orders.DELIVERY_IN_PROGRESS)) {
-//            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
-//        }
-//
-//        Orders orders = new Orders();
-//        orders.setId(ordersDB.getId());
-//        // 更新订单状态,状态转为完成
-//        orders.setStatus(Orders.COMPLETED);
-//        orders.setDeliveryTime(LocalDateTime.now());
-//
-//        orderMapper.update(orders);
-//    }
+    /**
+     * 完成订单
+     *
+     * @param id
+     */
+    public void complete(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在，并且状态为4
+        if (ordersDB == null || !ordersDB.getStatus().equals(Orders.DELIVERY_IN_PROGRESS)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Orders orders = new Orders();
+        orders.setId(ordersDB.getId());
+        // 更新订单状态,状态转为完成
+        orders.setStatus(Orders.COMPLETED);
+        orders.setDeliveryTime(LocalDateTime.now());
+
+        orderMapper.update(orders);
+    }
 
     /**
      * 客户催单
@@ -530,64 +530,4 @@ public class OrderService  {
     }
 
 
-//    /**
-//     * 订单支付
-//     *
-//     * @param ordersPaymentDTO
-//     * @return
-//     */
-//    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
-//        // 当前登录用户id
-//        Long userId = BaseContext.getCurrentId();
-//        User user = userMapper.getById(userId);
-//
-//        //调用微信支付接口，生成预支付交易单
-//        JSONObject jsonObject = weChatPayUtil.pay(
-//                ordersPaymentDTO.getOrderNumber(), //商户订单号
-//                new BigDecimal(0.01), //支付金额，单位 元
-//                "苍穹外卖订单", //商品描述
-//                user.getOpenid() //微信用户的openid
-//        );
-//
-//        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
-//            throw new OrderBusinessException("该订单已支付");
-//        }
-//
-//        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
-//        vo.setPackageStr(jsonObject.getString("package"));
-//
-//        return vo;
-//    }
-
-//    /**
-//     * 支付成功，修改订单状态
-//     *
-//     * @param outTradeNo
-//     */
-//    public void paySuccess(String outTradeNo) {
-//        // 当前登录用户id
-//        Long userId = BaseContext.getCurrentId();
-//
-//        // 根据订单号查询当前用户的订单
-//        Orders ordersDB = orderMapper.getByNumberAndUserId(outTradeNo, userId);
-//
-//        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
-//        Orders orders = Orders.builder()
-//                .id(ordersDB.getId())
-//                .status(Orders.TO_BE_CONFIRMED)
-//                .payStatus(Orders.PAID)
-//                .checkoutTime(LocalDateTime.now())
-//                .build();
-//
-//        orderMapper.update(orders);
-//
-//        //通过websocket向客户端浏览器推送消息 type orderId content
-//        Map map = new HashMap();
-//        map.put("type",1); // 1表示来单提醒 2表示客户催单
-//        map.put("orderId",ordersDB.getId());
-//        map.put("content","订单号：" + outTradeNo);
-//
-//        String json = JSON.toJSONString(map);
-//        webSocketServer.sendToAllClient(json);
-//    }
 }
